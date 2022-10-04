e87338801b1ad3d3d85593e19d5693faaaa39e97
Sala7din <17114499+Sala7din@users.noreply.github.com>
2022-10-04 21:22:15 +0200

    solved issue with text not appearing in fields andstyled text in password fields

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+let password1 = document.getElementById("field1")  
+let password2 = document.getElementById("field2")   
 
+function generatePasswords() {
 
-
-let function generatePasswords() {
+  password1.textContent = "Tesssst1"  
+  password2.textContent = "Tesssst2" 
     
 }
+
