9a1569758ee23ef5e1a670057befe0f0c8f12e11
Sala7din <17114499+Sala7din@users.noreply.github.com>
2022-10-04 21:40:28 +0200

    added for loop to generate random Password

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ let password2 = document.getElementById("field2")
 
 function generatePasswords() {
 
-  password1.textContent = Math.floor(Math.random() * length.characters)  
-  password2.textContent = Math.floor(Math.random() * length.characters)
+    for(let i = 0; i < 15; i++){
+        randomNum = Math.floor(Math.random() * length.characters)
+        // let randomPassword = characters[randomNum]
+        // characters[randomNum] += randomPassword
+    }
+  password1.textContent = randomPassword  
+  password2.textContent = randomPassword
     
 }
 
